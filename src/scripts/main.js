9bee50d57da795e09f286e8ae7c7d4632cde74b5
TiFAu <turchin.anton2@gmail.com>
2026-03-09 21:46:04 +0200

    added first solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const collectionLi = [...document.querySelectorAll('li')];
+const tree = document.querySelector('.tree');
+
+collectionLi.forEach((element) => {
+  const span = document.createElement('span');
+  // eslint-disable-next-line no-shadow
+  const parent = element.parentNode;
+
+  element.style.display = 'flex-block';
+  span.appendChild(element);
+  parent.appendChild(span);
+});
+
+// eslint-disable-next-line no-shadow
+tree.addEventListener(
+  'click',
+  // eslint-disable-next-line no-shadow
+  (event) => {
+    const link = event.target.closest('li');
+
+    if (!link) {
+      return;
+    }
+
+    if (link.style.display === 'none') {
+      link.style.display = 'box';
+    } else {
+      link.style.display = 'none';
+    }
+  },
+  { capture: true },
+);
