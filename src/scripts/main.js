fc2369708033be1703c9396076ac43e0a0f70c23
TiFAu <turchin.anton2@gmail.com>
2026-03-10 19:33:02 +0200

    added second solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,19 @@
 'use strict';
 
-const collectionLi = [...document.querySelectorAll('li')];
-const tree = document.querySelector('.tree');
+document
+  .querySelector('.tree')
+  .querySelectorAll('li')
+  .forEach((element) => {
+    const treeItem = element.querySelector('ul');
 
-collectionLi.forEach((element) => {
-  const span = document.createElement('span');
-  // eslint-disable-next-line no-shadow
-  const parent = element.parentNode;
+    if (treeItem) {
+      const span = document.createElement('span');
 
-  element.style.display = 'flex-block';
-  span.appendChild(element);
-  parent.appendChild(span);
-});
+      span.textContent = element.firstChild.textContent;
+      element.firstChild.replaceWith(span);
 
-// eslint-disable-next-line no-shadow
-tree.addEventListener(
-  'click',
-  // eslint-disable-next-line no-shadow
-  (event) => {
-    const link = event.target.closest('li');
-
-    if (!link) {
-      return;
+      span.addEventListener('click', () => {
+        treeItem.hidden = !treeItem.hidden;
+      });
     }
-
-    if (link.style.display === 'none') {
-      link.style.display = 'box';
-    } else {
-      link.style.display = 'none';
-    }
-  },
-  { capture: true },
-);
+  });
